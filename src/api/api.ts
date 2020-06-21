8b26237783e722e753cab4597bf0ffa41430d721
Marcin Swornowski <swor71@gmail.com>
2020-06-21 17:34:07 +0200

    Add initial api design and types for UserData

---
+++ src/api/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { UserData } from '../types/types';
+
+export class GitHubAPI {
+  private URL = 'https://api.github.com';
+
+  public fetchUserData(userName: string): Promise<UserData> {
+    const userData = axios
+      .get(`${this.URL}/users/${userName}`, {
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+        },
+      })
+      .then(res => res?.data);
+
+    return userData;
+  }
+}
