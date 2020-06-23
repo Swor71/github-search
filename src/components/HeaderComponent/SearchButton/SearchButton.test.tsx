d6eec66f377022e9dc36970e2674ba29c4abfb18
Marcin Swornowski <swor71@gmail.com>
2020-06-23 21:11:10 +0200

    Add check against onClick event

--- src/components/HeaderComponent/SearchButton/SearchButton.test.tsx
+++ src/components/HeaderComponent/SearchButton/SearchButton.test.tsx
@@ -1,16 +1,27 @@
-// import { renderHook } from '@testing-library/react-hooks';
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import { SearchButton } from './SearchButton';
 
 afterEach(cleanup);
 
 describe('SearchButton component', () => {
-  test('renders the component', () => {
+  it('renders the component', () => {
     const fn = jest.fn();
 
     const { getByText } = render(<SearchButton onHandleSearch={() => fn()} />);
 
     expect(getByText('Search')).toBeInTheDocument();
+  });
+
+  it('fires onClick event', () => {
+    const fn = jest.fn();
+
+    const { getByText } = render(<SearchButton onHandleSearch={() => fn()} />);
+
+    const button = getByText('Search');
+
+    fireEvent.click(button);
+
+    expect(fn).toHaveBeenCalledTimes(1);
   });
 });
