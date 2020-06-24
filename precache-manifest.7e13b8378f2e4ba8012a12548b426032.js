bccfaf35306f55d02aa53f21354e1b3263de0286
Marcin Swornowski <swor71@gmail.com>
2020-06-24 22:02:12 +0200

    Updates

---
+++ precache-manifest.7e13b8378f2e4ba8012a12548b426032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3baa6cdea6c83a8c835e238388ee38",
+    "url": "/github-search/index.html"
+  },
+  {
+    "revision": "736e307eb0cee354d45b",
+    "url": "/github-search/static/css/main.2b254262.chunk.css"
+  },
+  {
+    "revision": "ded42b7cf303bc24430a",
+    "url": "/github-search/static/js/2.cbd5d40f.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/github-search/static/js/2.cbd5d40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "736e307eb0cee354d45b",
+    "url": "/github-search/static/js/main.68f06f3c.chunk.js"
+  },
+  {
+    "revision": "7ba9a64313dfe8d279be",
+    "url": "/github-search/static/js/runtime-main.5f638c5b.js"
+  }
+]);
