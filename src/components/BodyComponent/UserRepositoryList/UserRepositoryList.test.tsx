604b00ea371aefd289deeb302e4a65dd5b0e233e
Marcin Swornowski <swor71@gmail.com>
2020-07-03 18:30:03 +0200

    Fix UserRepositoryList compoonent tests after introducing mobx and removing props

--- src/components/BodyComponent/UserRepositoryList/UserRepositoryList.test.tsx
+++ src/components/BodyComponent/UserRepositoryList/UserRepositoryList.test.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { UserRepositoryList } from './UserRepositoryList';
 import { mockRepositoryData } from '../../../mock/mockData';
+import { StoreContext } from '../../../store/store';
 
 afterEach(cleanup);
 
 describe('UserRepositoryList component', () => {
   it('renders the component', () => {
-    const { getByText } = render(<UserRepositoryList repositories={mockRepositoryData} />);
+    const { getByText } = render(
+      <StoreContext.Provider value={{ ...StoreContext, userRepositories: mockRepositoryData, loading: false }}>
+        <UserRepositoryList />
+      </StoreContext.Provider>,
+    );
 
     const repoListHeader = getByText('Top repositories');
 
@@ -15,7 +20,11 @@ describe('UserRepositoryList component', () => {
   });
 
   it('renders list of three repositories', () => {
-    const { queryAllByTestId } = render(<UserRepositoryList repositories={mockRepositoryData} />);
+    const { queryAllByTestId } = render(
+      <StoreContext.Provider value={{ ...StoreContext, userRepositories: mockRepositoryData, loading: false }}>
+        <UserRepositoryList />
+      </StoreContext.Provider>,
+    );
 
     const repositoryList = queryAllByTestId('link');
 
